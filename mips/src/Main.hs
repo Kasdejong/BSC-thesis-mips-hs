c92adbd533cb9c4980e3fcb45626a3ca015685bc
Kasdejong <k.j2.de.jong@student.vu.nl>
2025-04-16 10:00:04 +0200

    WIP

--- mips/src/Main.hs
+++ mips/src/Main.hs
@@ -3,92 +3,192 @@ module Main (main) where
 -- subset of MIPS instructions, (add, mov, lw, sw)
 
 data Instruction = 
-    ADD Reg Reg |
-    MOV Reg Reg |
-    LW Reg Int |
-    SW Reg Int |
-    NOP
+    INSTR_ADD RegID RegID RegID |
+    INSTR_MOV RegID RegID |
+    INSTR_LW RegID Int |
+    INSTR_SW RegID Int |
+    INSTR_NOP
   deriving (Show, Eq)
   
-data Reg = 
+data Operation = OP_ADD | OP_LW | OP_SW | OP_MV deriving (Show, Eq, Enum)
+  
+data RegID = 
     R0 | R1 | R2 | R3 | R4 | R5 | R6 | R7
   deriving (Show, Eq, Enum)
   
+data RegValue = 
+    Val Int | Pending RSID
+  deriving (Show, Eq)
+  
+data RSID = RS_ADD1 | RS_ADD2 | RS_LS1 deriving (Show, Eq, Enum)
+  
+data ResStation = ResStation {
+    op :: Operation,
+    vj :: Int,
+    vk :: Int,
+    qj :: Maybe RSID,
+    qk :: Maybe RSID,
+    a :: Int,
+    busy :: Bool
+  } deriving (Show, Eq)
+  
 sampleInstructions :: [Instruction]
 sampleInstructions = 
-  [ ADD R1 R2
-  , MOV R2 R4
-  , LW R3 4
-  , SW R4 4
-  , NOP
+  [ INSTR_ADD R1 R1 R2
+  , INSTR_ADD R2 R3 R4
+  , INSTR_MOV R2 R4
+  , INSTR_LW R3 4
+  , INSTR_SW R4 4
+  , INSTR_NOP
   ]
   
--- A simple function to execute the instructions
-executeInstruction :: CPU -> Instruction -> IO CPU
-executeInstruction cpu (ADD r1 r2) = do 
-  putStrLn ("Adding " ++ show r1 ++ " and " ++ show r2)
-  return (doTick cpu (ADD r1 r2))
-executeInstruction cpu (MOV r1 r2) = do
-  putStrLn ("Moving " ++ show r1 ++ " to " ++ show r2)
-  return (doTick cpu (MOV r1 r2))
-executeInstruction cpu (LW r i) = do
-  putStrLn ("Loading word from memory address " ++ show i ++ " to register " ++ show r)
-  return (doTick cpu (LW r i))
-executeInstruction cpu (SW r i) = do
-  putStrLn ("Storing word from register " ++ show r ++ " to memory address " ++ show i)
-  return (doTick cpu (SW r i))
-executeInstruction cpu NOP = do
-  putStrLn "No operation"
-  return (doTick cpu NOP)
+chooseStation :: CPU -> Instruction -> Maybe RSID
+chooseStation cpu (INSTR_ADD _ _ _) = 
+  if not (busy (stations cpu !! fromEnum RS_ADD1)) 
+  then Just RS_ADD1 
+  else if not (busy (stations cpu !! fromEnum RS_ADD2)) 
+       then Just RS_ADD2 
+       else Nothing
+chooseStation cpu (INSTR_LW _ _) = 
+  if not (busy (stations cpu !! fromEnum RS_LS1)) 
+  then Just RS_LS1 
+  else Nothing
+chooseStation cpu (INSTR_SW _ _) = 
+  if not (busy (stations cpu !! fromEnum RS_LS1)) 
+  then Just RS_LS1 
+  else Nothing
+chooseStation _ _ = error "Invalid instruction for reservation station"
+  
+issueInstruction :: CPU -> Instruction -> IO (Maybe CPU)
+issueInstruction cpu instr = do
+  putStrLn ("Issuing instruction " ++ show instr)
+  newCpu <- (case chooseStation cpu instr of
+    Just stationID -> do
+      let newStation = case instr of
+            INSTR_ADD rd r1 r2 -> createAddStation cpu r1 r2
+            INSTR_LW r1 addr    -> createLoadStation cpu instr
+            INSTR_SW r1 addr    -> createLoadStation cpu instr
+            _                   -> error "Invalid instruction for reservation station"
+      let newStations = writeToSeq (stations cpu) (fromEnum stationID) newStation
+      let newCpu = case instr of
+            INSTR_ADD rd _ _ -> cpu { stations = newStations, reg = writeToSeq (reg cpu) (fromEnum rd) (Pending stationID) }
+            INSTR_LW rd _    -> cpu { stations = newStations, reg = writeToSeq (reg cpu) (fromEnum rd) (Pending stationID) }
+            _                -> cpu { stations = newStations }
+      putStrLn ("Issued to station " ++ show stationID)
+      return (Just newCpu)
+    Nothing -> do
+      putStrLn "No available reservation stations for operation"
+      return Nothing)
+  return newCpu
+
+
+createAddStation :: CPU -> RegID -> RegID -> ResStation
+createAddStation cpu r1 r2 = ResStation {
+  op = OP_ADD,
+  vj = case state1 of
+    Val v -> v
+    Pending _ -> 0,
+  vk = case state2 of
+    Val v -> v
+    Pending _ -> 0,
+  qj = case state1 of
+    Val _ -> Nothing
+    Pending rsid -> Just rsid,
+  qk = case state2 of
+    Val _ -> Nothing
+    Pending rsid -> Just rsid,
+  a = 0,
+  busy = True
+} where 
+  state1 = mapRegIDToValue cpu r1
+  state2 = mapRegIDToValue cpu r2
+  
+createLoadStation :: CPU -> Instruction -> ResStation
+createLoadStation cpu (INSTR_LW r1 addr) = ResStation {
+  op = OP_LW,
+  qj = case state1 of
+    Val _ -> Nothing
+    Pending rsid -> Just rsid,
+  qk = Nothing,
+  vj = case state1 of
+    Val v -> v
+    Pending _ -> 0,
+  vk = 0,
+  a = addr,
+  busy = True
+} where state1 = mapRegIDToValue cpu r1
+createLoadStation cpu (INSTR_SW r1 addr) = ResStation {
+  op = OP_SW,
+  qj = case state1 of
+    Val _ -> Nothing
+    Pending rsid -> Just rsid,
+  qk = case state1 of
+    Val _ -> Nothing
+    Pending rsid -> Just rsid,
+  vj = case state1 of
+    Val v -> v
+    Pending _ -> 0,
+  vk = case state1 of
+    Val v -> v
+    Pending _ -> 0,
+  a = addr,
+  busy = True
+} where state1 = mapRegIDToValue cpu r1
 
 -- A simple function to execute a list of instructions
 executeInstructions :: CPU -> [Instruction] -> IO CPU
 executeInstructions cpu [] = return cpu
 executeInstructions cpu (instr:instrs) = do
-  newCpu <- executeInstruction cpu instr
-  executeInstructions newCpu instrs
+  newCpu <- (case instr of
+    INSTR_NOP -> return (Just cpu)
+    INSTR_MOV r1 r2 -> do
+      let newCpu = cpu { reg = writeToSeq (reg cpu) (fromEnum r1) (mapRegIDToValue cpu r2) }
+      return (Just newCpu)
+    _ -> do
+      newCpu <- issueInstruction cpu instr 
+      return newCpu)
+  case newCpu of
+    Nothing -> return (executeInstructions (runReservationStations cpu) instrs)
+    Just newCpu' -> return (executeInstructions newCpu instrs)
+    
+-- A function to run the reservation stations
+runReservationStations :: CPU -> IO CPU
+runReservationStations cpu = do
+  let stations = stations cpu
+  let newStations = map (updateStation cpu) stations
+  let newCpu = cpu { stations = newStations }
+  return newCpu
+  
+-- A function to update the reservation stations
+updateStation :: CPU -> ResStation -> ResStation
+updateStation cpu station = 
+  case op station of
+    OP_ADD -> if busy station then station { busy = False } else station
+    OP_LW -> if busy station then station { busy = False } else station
+    OP_SW -> if busy station then station { busy = False } else station
+    _ -> error "Invalid operation in reservation station"
   
 data CPU = 
-    CPU { reg :: [Int], 
+    CPU { reg :: [RegValue], 
+          stations :: [ResStation],
           pc :: Int, 
           mem :: [Int] }
   deriving (Show) 
   
+-- Initialize the reservation stations
+initResStation :: ResStation
+initResStation = ResStation { op = OP_ADD, vj = 0, vk = 0, qj = Nothing, qk = Nothing, a = 0, busy = False }
+  
 -- Initialize the CPU with some registers and memory
 initCPU :: CPU
-initCPU = CPU { reg = [2, 3, 4, 5, 6, 7, 8, 9], pc = 0, mem = replicate 64 0 }
+initCPU = CPU { reg = [Val 2, Val 3, Val 4, Val 5, Val 6, Val 7, Val 8, Val 9], pc = 0, mem = replicate 64 0, stations = [initResStation, initResStation, initResStation] }
 
 -- map registers to their values
-mapRegToValue :: CPU -> Reg -> Int
-mapRegToValue cpu R0 = (reg cpu) !! 0
-mapRegToValue cpu R1 = (reg cpu) !! 1
-mapRegToValue cpu R2 = (reg cpu) !! 2
-mapRegToValue cpu R3 = (reg cpu) !! 3
-mapRegToValue cpu R4 = (reg cpu) !! 4
-mapRegToValue cpu R5 = (reg cpu) !! 5
-mapRegToValue cpu R6 = (reg cpu) !! 6
-mapRegToValue cpu R7 = (reg cpu) !! 7
+mapRegIDToValue :: CPU -> RegID -> RegValue
+mapRegIDToValue cpu r = (reg cpu) !! fromEnum r
 
--- Update the CPU state based on the instruction
-doTick :: CPU -> Instruction -> CPU
-doTick cpu (ADD r1 r2) = 
-  let val1 = mapRegToValue cpu r1
-      val2 = mapRegToValue cpu r2
-      newReg = take (fromEnum r1) (reg cpu) ++ [val1 + val2] ++ drop (fromEnum r1 + 1) (reg cpu)
-  in cpu { reg = newReg, pc = pc cpu + 1 }
-doTick cpu (MOV r1 r2) =
-  let val = mapRegToValue cpu r1
-      newReg = take (fromEnum r2) (reg cpu) ++ [val] ++ drop (fromEnum r2 + 1) (reg cpu)
-  in cpu { reg = newReg, pc = pc cpu + 1 }
-doTick cpu (LW r i) =
-  let val = mem cpu !! i
-      newReg = take (fromEnum r) (reg cpu) ++ [val] ++ drop (fromEnum r + 1) (reg cpu)
-  in cpu { reg = newReg, pc = pc cpu + 1 }
-doTick cpu (SW r i) =
-  let val = mapRegToValue cpu r
-      newMem = take i (mem cpu) ++ [val] ++ drop (i + 1) (mem cpu)
-  in cpu { mem = newMem, pc = pc cpu + 1 }
-doTick cpu NOP = cpu { pc = pc cpu + 1 }
+writeToSeq :: [a] -> Int -> a -> [a]
+writeToSeq xs i x = take i xs ++ [x] ++ drop (i + 1) xs
  
 -- Main function to run the program
 main :: IO ()
